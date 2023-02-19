62488351aaf76f53cc0bf7a336ed1b2885b7a94d
seleza1 <koposov.serezha@gmail.com>
2023-02-19 17:56:31 +0300

    succes

--- SettingsUserDefaults/ViewController.swift
+++ SettingsUserDefaults/ViewController.swift
@@ -9,13 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var ofOnswitch: UISwitch!
+    @IBOutlet var ofOnSwitch: UISwitch!
+    let onOfKey = "onOfKey"
+    let userDefaults = UserDefaults.standard
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        changeSwitch()
 
     }
 
     @IBAction func ofOnSwitchChange(_ sender: UISwitch) {
+        if ofOnSwitch.isOn {
+            userDefaults.set(true, forKey: onOfKey)
+            view.backgroundColor = .gray
+        } else {
+            userDefaults.set(false, forKey: onOfKey)
+            view.backgroundColor = .blue
 
+        }
+    }
+
+    private func changeSwitch() {
+        if userDefaults.bool(forKey: onOfKey) {
+            ofOnSwitch.setOn(true, animated: false)
+            view.backgroundColor = .gray
+        } else {
+            ofOnSwitch.setOn(false, animated: true)
+            view.backgroundColor = .blue
+
+        }
     }
 }
