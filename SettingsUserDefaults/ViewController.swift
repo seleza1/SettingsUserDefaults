44eb3c7802e7330ec0475e0e85884f722f2e6ed3
seleza1 <koposov.serezha@gmail.com>
2023-02-19 12:11:16 +0300

    create func updateTheme

--- SettingsUserDefaults/ViewController.swift
+++ SettingsUserDefaults/ViewController.swift
@@ -15,25 +15,30 @@ class ViewController: UIViewController {
     
     let userDefaults = UserDefaults.standard
     let onOfKey = "onOffKey"
+    let themeKey = "themeKey"
+    let darkTheme = "darkTheme"
+    let lightTheme = "lightTheme"
+    let redTheme = "redTheme"
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         checkSwitchState()
     }
     
-    func checkSwitchState() {
-        if userDefaults.bool(forKey: onOfKey) {
-            onOfSwitch.setOn(true, animated: false)
-            onOfLabel.text = "ON"
-        } else {
-            onOfSwitch.setOn(false, animated: false)
-            onOfLabel.text = "OFF"
-        }
-        
-    }
-    
     @IBAction func segmentedChanged(_ sender: UISegmentedControl) {
+        switch themeSegment.selectedSegmentIndex {
+        case 0:
+            userDefaults.set(lightTheme, forKey: themeKey)
+        case 1:
+            userDefaults.set(darkTheme, forKey: themeKey)
+        case 2:
+            userDefaults.set(redTheme, forKey: themeKey)
+        default:
+            userDefaults.set(lightTheme, forKey: themeKey)
 
+        }
+        updateTheme()
     }
     
     @IBAction func switchChanged(_ sender: UISwitch) {
@@ -46,5 +51,29 @@ class ViewController: UIViewController {
         }
         
     }
-    
+
+    private func checkSwitchState() {
+        if userDefaults.bool(forKey: onOfKey) {
+            onOfSwitch.setOn(true, animated: false)
+            onOfLabel.text = "ON"
+        } else {
+            onOfSwitch.setOn(false, animated: false)
+            onOfLabel.text = "OFF"
+        }
+    }
+
+    private func updateTheme() {
+        let theme = userDefaults.string(forKey: themeKey)
+
+        if theme == lightTheme {
+            themeSegment.selectedSegmentIndex = 0
+            view.backgroundColor = .white
+        } else if theme == darkTheme {
+            themeSegment.selectedSegmentIndex = 1
+            view.backgroundColor = .gray
+        } else if theme == redTheme {
+            themeSegment.selectedSegmentIndex = 2
+            view.backgroundColor = .red
+        }
+    }
 }
