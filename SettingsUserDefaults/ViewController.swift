7b7f7b86e650d9ed72b4a0a271b2dec6b9a1e6c7
seleza1 <koposov.serezha@gmail.com>
2023-02-19 10:06:57 +0300

    Initial Commit

---
+++ SettingsUserDefaults/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingsUserDefaults
+//
+//  Created by user on 19.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
