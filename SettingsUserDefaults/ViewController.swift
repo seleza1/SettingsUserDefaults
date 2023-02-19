76157dd47c089a1d6d5801425fd8c173669c21eb
seleza1 <koposov.serezha@gmail.com>
2023-02-19 10:15:49 +0300

    layout up

--- SettingsUserDefaults/ViewController.swift
+++ SettingsUserDefaults/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var onOfLabel: UILabel!
+
+    @IBOutlet var onOfSwitch: UISwitch!
+    @IBOutlet var themeSegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
